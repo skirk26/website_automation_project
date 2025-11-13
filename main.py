95f6a18e8e79d77ae28484583904fdb20c3e7f4d
Sam Kirkhorn <sam.kirkhorn.23@cnu.edu>
2025-11-12 21:22:45 -0500

    pop up fix

--- main.py
+++ main.py
@@ -3,9 +3,18 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_experimental_option("prefs", {
+    "credentials_enable_service": False,
+    "profile.password_manager_enabled": False
+})
+chrome_options.add_experimental_option("prefs", {
+    "profile.password_manager_leak_detection": False
+})
 
 service = Service("/Users/samkirkhorn/desktop/chromedriver-mac-arm64/chromedriver")
-driver = webdriver.Chrome(service=service)
+driver = webdriver.Chrome(service=service,options=chrome_options)
 wait = WebDriverWait(driver, 10)
 def click(element):
     wait.until(EC.element_to_be_clickable((By.ID, element))).click()
@@ -15,16 +24,17 @@ def send_keys(element, key):
     wait.until(EC.element_to_be_clickable((By.ID, element))).send_keys(key)
 
 driver.get("https://www.saucedemo.com/")
-driver.implicitly_wait(10)
+
 
 driver.find_element(By.ID, "user-name").send_keys('standard_user')
 driver.find_element(By.ID, "password").send_keys('secret_sauce')
 driver.find_element(By.ID, "login-button").click()
+
 click("add-to-cart-sauce-labs-backpack")
 click("add-to-cart-sauce-labs-bolt-t-shirt")
 click("shopping_cart_container")
-check_presence('checkout')
-click("checkout")
+click('checkout')
+
 check_presence('first-name')
 send_keys('first-name', 'John')
 check_presence('last-name')
@@ -33,5 +43,7 @@ check_presence('postal-code')
 send_keys('postal-code', '123456')
 check_presence('continue')
 click("continue")
+item_total = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "summary_subtotal_label")))
+print(item_total.text)
 driver.quit()
 
