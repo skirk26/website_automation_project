d340178b45a478a34e99095561be5c603d66c9f5
Sam Kirkhorn <sam.kirkhorn.23@cnu.edu>
2025-11-12 20:37:51 -0500

    checkout

--- main.py
+++ main.py
@@ -9,14 +9,29 @@ driver = webdriver.Chrome(service=service)
 wait = WebDriverWait(driver, 10)
 def click(element):
     wait.until(EC.element_to_be_clickable((By.ID, element))).click()
-try:
-    driver.get("https://www.saucedemo.com/")
-    driver.implicitly_wait(10)
+def check_presence(element):
+    wait.until(EC.presence_of_element_located((By.ID, element)))
+def send_keys(element, key):
+    wait.until(EC.element_to_be_clickable((By.ID, element))).send_keys(key)
 
-    driver.find_element(By.ID, "user-name").send_keys('standard_user')
-    driver.find_element(By.ID, "password").send_keys('secret_sauce')
-    driver.find_element(By.ID, "login-button").click()
-    click("add-to-cart-sauce-labs-backpack")
-    input('test')
-except:
-    driver.quit()
+driver.get("https://www.saucedemo.com/")
+driver.implicitly_wait(10)
+
+driver.find_element(By.ID, "user-name").send_keys('standard_user')
+driver.find_element(By.ID, "password").send_keys('secret_sauce')
+driver.find_element(By.ID, "login-button").click()
+click("add-to-cart-sauce-labs-backpack")
+click("add-to-cart-sauce-labs-bolt-t-shirt")
+click("shopping_cart_container")
+check_presence('checkout')
+click("checkout")
+check_presence('first-name')
+send_keys('first-name', 'John')
+check_presence('last-name')
+send_keys('last-name', 'Doe')
+check_presence('postal-code')
+send_keys('postal-code', '123456')
+check_presence('continue')
+click("continue")
+driver.quit()
+
