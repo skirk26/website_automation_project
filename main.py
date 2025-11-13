eeb2d5d2564ced0436ca5cc69060e9e0e4295c1d
Sam Kirkhorn <sam.kirkhorn.23@cnu.edu>
2025-11-12 20:09:29 -0500

    defintions

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+service = Service("/Users/samkirkhorn/desktop/chromedriver-mac-arm64/chromedriver")
+driver = webdriver.Chrome(service=service)
+wait = WebDriverWait(driver, 10)
